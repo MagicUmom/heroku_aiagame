9e27cc53ec0eebca54fffa529bf5adedd99548fb
PEPE <ken51006b@hotmail.com>
2020-01-15 17:22:19 +0800

    .

--- aia/production_settings.py
+++ aia/production_settings.py
@@ -16,4 +16,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Turn off DEBUG mode.
-DEBUG = False
+DEBUG = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
